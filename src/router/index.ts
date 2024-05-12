5e4af98fde17dd9b8e8787a92ca2d3af88d0ef75
ojagao <ahiroto1234@gmail.com>
2024-05-12 20:41:23 +0900

    Fix Rotuer to,from,savePosition

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior() {
     return { top: 0 };
   }
 });
